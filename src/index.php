2bb2afe3c55a0e780a5bfe2997b97faf6f81e20e
Nassim YAZI <78474187+Nassim-dev@users.noreply.github.com>
2021-10-28 14:30:34 +0200

    Fight Club

--- src/index.php
+++ src/index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Small Game - Fight</title>
 </head>
 <body>
-    <h1>HElllo</h1>
+    <h1>Fithing Club</h1>
+    
 </body>
 </html>
